8412ec56c82be0def8b9d20ccb7cde0f1418db28
Matt <166550744+wewebasha1@users.noreply.github.com>
2026-02-07 14:13:52 -0500

    added login and menu screens

--- app.py
+++ app.py
@@ -8,36 +8,81 @@ import plotly.express as px
 
 #--------------------------------CONSTANTS-----------------------------------
 # screens
-MENU = 0
+LOGIN = 0
+MENU = 1
+GRAPH_DISPLAY = 2
 # other
-TEMP_THRESHOLD = 0
+LOW_TEMP_THRESHOLD = 20
+HI_TEMP_THRESHOLD = 30
 HUM_THRESHOLD = 60
+
+#----------------------------------LOGIN------------------------------------
+def check_password():
+
+    def password_entered():
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("Password incorrect")
+        return False
+    else:
+        return True
+
+#----------------------------------CALLBACKS------------------------------------
+
+def device_display(name):
+    st.session_state.screen = GRAPH_DISPLAY
+
 
 #--------------------------------SESSION VALS--------------------------------
 
 if "screen" not in st.session_state:
-    st.session_state.screen = 0
+    st.session_state.screen = LOGIN
+
+if "kits" not in st.session_state:
+    st.session_state.kits = ["Lexus Monitor", "Porsche Monitor", "Truck Monitor"]
 
 #---------------------------------SCREENS------------------------------------
 
-if st.session_state.screen == MENU:
+if st.session_state.screen == LOGIN:
+    st.title("Please enter your password")
+    if check_password():
+        st.session_state.screen = MENU
+        st.rerun()
+
+elif st.session_state.screen == MENU:
+    st.title("Please select your device")
+    with st.container(horizontal=True):
+        for i, name in enumerate(st.session_state.kits):
+            st.button(name, on_click=device_display, args=(name,))
+
+
+elif st.session_state.screen == GRAPH_DISPLAY:
     st.set_page_config(layout='wide')
     st.title("Dashboard")
 
     data = pd.read_csv("data\data.csv")
 
-    # Find where the line crosses the threshold and add to plot
-    crossings = data[( (data['humidity_pct'] >= HUM_THRESHOLD) & (data['humidity_pct'].shift(1) < HUM_THRESHOLD) ) |
-                ( (data['humidity_pct'] <= HUM_THRESHOLD) & (data['humidity_pct'].shift(1) > HUM_THRESHOLD) )]
+    #-----------------Humidity Display-----------------
 
     fig = px.line(data, x='timestamp', y='humidity_pct', title='Humidity')
+    
+    st.plotly_chart(fig)
 
-    fig.add_hline(y=HUM_THRESHOLD, line_dash="dash", line_color="red")
+    #-----------------Temperature Display-----------------
 
-    fig.add_scatter(x=crossings['timestamp'], y=crossings['humidity_pct'], 
-                    mode='markers', marker=dict(color='red', size=10), 
-                    name='Crossings')
-
-    # plot
+    fig = px.line(data, x='timestamp', y='temperature_c', title='Temperature')
     
     st.plotly_chart(fig)
